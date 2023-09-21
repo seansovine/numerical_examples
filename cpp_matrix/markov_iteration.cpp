a6b6c5412d5a679293ebfa5893b6ed25808335cd
Sean Sovine <sean.r.sovine@gmail.com>
2023-09-21 16:50:56 -0400

    Prettier output in markov chain example

--- cpp_matrix/markov_iteration.cpp
+++ cpp_matrix/markov_iteration.cpp
@@ -3,13 +3,22 @@
 
 int main()
 {
+	std::cout << std::fixed << std::setprecision(PRECISION);
+	std::string header{"Markov chain steady-state iteration:"};
+	std::cout << header << std::endl
+			  << std::endl;
+
 	Matrix<double> m{2, 2};
 	m(0, 0) = 0.45;
 	m(0, 1) = 0.55;
 	m(1, 0) = 0.4;
 	m(1, 1) = 0.6; // Tends to enter state 1 and stay there.
 
-	std::cout << "Inf norm of m is: " << infNorm(m) << std::endl;
+	std::cout << "Transition matrix m =" << std::endl
+			  << std::string(m) << std::endl
+			  << std::endl;
+	std::cout << "Inf norm of m is: " << infNorm(m) << std::endl
+			  << std::endl;
 
 	double tolerance = 1.0E-12;
 	Matrix<double> curr{m};
