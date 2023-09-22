7208fd6cbb738a87a0f02a35a39227b1258c5e0c
Sean Sovine <sean.r.sovine@gmail.com>
2023-09-22 10:13:37 -0400

    Tweak test script output

--- cpp_matrix/markov_iteration.cpp
+++ cpp_matrix/markov_iteration.cpp
@@ -35,7 +35,11 @@ int main() {
     }
   }
 
+  std::cout << std::scientific
+            << "Convergence stopping threshold: " << tolerance
+            << std::defaultfloat << std::endl;
   std::cout << "Approximate convergence after " << num_iter << " iterations."
+            << std::endl
             << std::endl;
   std::cout << "Approximate 1 * π for steady state π: " << std::endl
             << std::string(curr) << std::endl;
