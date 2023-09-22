f687e9d59bd50eefa694f2e132b0cba125d59c9a
Sean Sovine <sean.r.sovine@gmail.com>
2023-09-22 10:32:47 -0400

    Oops: need abs value in norm!

--- cpp_matrix/matrix_lib/utils.hpp
+++ cpp_matrix/matrix_lib/utils.hpp
@@ -10,7 +10,7 @@ template <typename T> double infNorm(const Matrix<T> &m) {
   for (unsigned i = 0; i < m.rows; i++)
     for (unsigned j = 0; j < m.cols; j++) {
       double d_val = static_cast<double>(m(i, j));
-      norm = std::max(norm, d_val);
+      norm = std::max(norm, std::abs(d_val));
     }
   return norm;
 }
