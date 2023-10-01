e7c1c67714b8e9b4d7c492db31726e2b7d977b56
Sean Sovine <sean.r.sovine@gmail.com>
2023-10-01 19:38:56 -0400

    Check filetype and exit safely on failure

--- image_proc/convert_gs.c
+++ image_proc/convert_gs.c
@@ -11,9 +11,12 @@
 const char *FILENAME = "sample.bmp";
 
 int main(void) {
-  FILE *fp;
+  // Declare dynamic variables.
+  FILE *fp = NULL;
   BMPHeader *bmpHeader = NULL;
   BMPInfoHeader *infoHeader = NULL;
+  PixelData *pixData = NULL;
+  PixelData *gsPixData = NULL;
 
   if ((fp = fopen(FILENAME, "rb")) == NULL) {
     printf("Cannot open file: %s\n\n", FILENAME);
@@ -26,15 +29,18 @@ int main(void) {
   printHeaderInfo(bmpHeader, infoHeader);
 
   // For now we only support 24-bit pixels.
-  assert(infoHeader->bitcount == 24);
+  if (strcmp(bmpHeader->type, "BM") != 0 || infoHeader->bitcount != 24) {
+    printf("Only 24-bit BMP files are accepted.");
+    // We still need to deallocate some vars.
+  } else {
+    // Read pixel data and convert to grayscale.
+    pixData = readPixels(bmpHeader, infoHeader, fp);
+    gsPixData = convertToGrayscale(pixData);
 
-  // Read pixel data and convert to grayscale.
-  PixelData *pixData = readPixels(bmpHeader, infoHeader, fp);
-  PixelData *gsPixData = convertToGrayscale(pixData);
-
-  // Write result to "<FILENAME_NO_EXT>_grayscale.bmp";
-  writeImageFile(fp, bmpHeader, infoHeader, gsPixData, FILENAME,
-                 strlen(FILENAME));
+    // Write result to "<FILENAME_NO_EXT>_grayscale.bmp";
+    writeImageFile(fp, bmpHeader, infoHeader, gsPixData, FILENAME,
+                   strlen(FILENAME));
+  }
 
   // Clean-up.
   free(pixData->pixels);
